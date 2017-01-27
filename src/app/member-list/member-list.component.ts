5001bc637d0aad60d0d4c75e85b26b413e05e33f
Margie Nevarez <margie.ynm@gmail.com>
2017-01-27 13:56:11 -0800

    implement a service to retrieve data from firebase / display list of members

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService} from '../member.service';
+import { Member } from '../member.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
+  providers: [MemberService]
 })
 export class MemberListComponent implements OnInit {
-
-  constructor() { }
+  members: FirebaseListObservable<any[]>;
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
 }
